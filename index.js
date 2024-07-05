e8f50c4cbf70e21084507315d2853677287c4253
Ivan <ivankist2@gmail.com>
2024-07-05 11:46:21 +0300

    checkAuth correction

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import multer from 'multer';
 
 import mongoose from 'mongoose';
 
@@ -19,11 +20,28 @@ mongoose
 
 const app = express();
 
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads');
+    },
+    fileName: (_, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage });
+
 app.use(express.json());
 
 app.post('/auth/login', Validators.loginValidation, UserController.login);
 app.post('/auth/register', Validators.registerValidation, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`,
+    });
+});
 
 app.get('/posts', PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -1,22 +1,28 @@
 import jwt from 'jsonwebtoken';
+import UserModel from '../models/User.js';
 
-export default (req, res, next) => {
+export default async (req, res, next) => {
     const token = (req.headers.authorization || '').replace(/Bearer\s/, '');
 
     if (token) {
         try {
             const decoded = jwt.verify(token, 'secret123');
-
+            const user = await UserModel.findById(decoded._id);
+            if (!user) {
+                return res.status(404).json({
+                    message: 'Access denied. User not found.',
+                });
+            }
             req.userId = decoded._id;
             next();
         } catch (err) {
             return res.status(403).json({
-                message: 'Access denied',
+                message: 'Access denied. Bad token.',
             });
         }
     } else {
         return res.status(403).json({
-            message: 'Access denied',
+            message: 'Access denied. There isn\'t token.',
         });
     }
 }
